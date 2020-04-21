85974d447e238df0232c56703e2a77e26b54a3cf
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-21 13:12:31 +0000

    Update export_db_no_data.sh

--- export_db_no_data.sh
+++ export_db_no_data.sh
@@ -31,7 +31,7 @@ Read_DB_Name() {
 
 
 for f in PATT_UTILS/sql/*; do
-script_name=$(echo $f| cut -d'/' -f 2)
+script_name=$(echo $f| cut -d'/' -f 3)
 DB_NAME_IN_SCRIPT_UPPERCASE=`Read_DB_Name $f `
 count=${#list_database_in_script[@]}
 flag1=""
