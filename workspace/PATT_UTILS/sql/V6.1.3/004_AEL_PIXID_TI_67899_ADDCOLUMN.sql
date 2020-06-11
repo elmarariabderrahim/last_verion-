d2843e5596efe45ccaf96802bf4a17b093a2b59c
abderahim <elmarari.abder@gmail.com>
2020-06-11 21:00:32 +0100

    add new

---
+++ workspace/PATT_UTILS/sql/V6.1.3/004_AEL_PIXID_TI_67899_ADDCOLUMN.sql
@@ -0,0 +1,5 @@
+use pixid;
+
+create taable cafe(id int);
+create table restaux(id int);
+commit;
---
+++ workspace/PATT_UTILS/sql/V6.1.3/200_AEL_DWHSTAGE_TI_67899_INSERTION.sql
@@ -0,0 +1,6 @@
+use dwhstage;
+
+insert into car vaalues (250);
+insert into car values (322);
+insert into car values (3);
+commit;
